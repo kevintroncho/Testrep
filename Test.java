22e94b8f9890d5340bc50bef92610eee605a02bb
kevintroncho <kevintroncho@iesmontsia.org>
2023-04-13 16:47:55 +0200

    Prova2

--- Test.java
+++ Test.java
@@ -1,1 +1,35 @@
-i8gasda
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class PlanetaTest {    
+
+    @Test 
+    public void testPlanetaEnano()
+    {     
+
+        //assertEquals(expected, actual);
+        //assertEquals(expected, actual);
+
+    }    
+
+    @Test 
+    public void testExpansio()
+    {
+
+        //assertEquals(expected, actual);
+
+    }
+
+    @Test 
+    public void testColisio()
+    {        
+
+        //assertEquals(expected, actual);
+        //assertEquals(expected, actual);
+        //assertEquals(expected, actual);
+        //assertNotEquals(unexpected, actual);              
+
+    }    
+}
